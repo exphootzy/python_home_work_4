b8f4858526a13ee604dc8044b131e06825494bd6
exphootzy <enjoyphoto@mail.ru>
2023-07-31 18:38:05 +0900

    2nd task completed

--- task_2.py
+++ task_2.py
@@ -2,19 +2,15 @@
 # Эти кусты обладают разной урожайностью, поэтому ко времени сбора на них выросло различное число ягод — на i-ом кусте выросло ai ягод.
 # В этом фермерском хозяйстве внедрена система автоматического сбора черники. Эта система состоит из управляющего модуля и нескольких собирающих модулей. Собирающий модуль за один заход, находясь непосредственно перед некоторым кустом, собирает ягоды с этого куста и с двух соседних с ним.
 # Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки.
+
 from random import randint
 
-n=(int(input("Введите число для n элементов для первого множества: ")))
-list_one =set([randint(1, 10) for x in range(1, n+1)])
-print(list_one)
+numbers_shrubs=(int(input("Введите число кустов: ")))
+list_bluebeyrry = ([randint(5, 9) for x in range(1, numbers_shrubs+1)])
+print(f"На каждом из {numbers_shrubs} кустов, находится следующее колличество ягод: {list_bluebeyrry}")
 
-n_bushes = int(input('Введите количество кустов черники: '))
-arr = list()
-for i in range(n_bushes):
-    a =int(input('Введите количество ягод на кусте: '))
-    arr.append(a)
-
-arr_count = list()
-for i in range(len(arr)):
-       arr_count.append(arr[i-2] + arr[i-1] + arr[i])
-print(max(arr_count))
+list_summ = list()
+for i in range(len(list_bluebeyrry)):
+       list_summ.append(list_bluebeyrry[i-2] + list_bluebeyrry[i-1] + list_bluebeyrry[i])
+print(f"Суммы близрастущих 3ех кустов {list_summ}")
+print(f"Максимальная сумма близрастущих 3ех кустов {max(list_summ)}")       
